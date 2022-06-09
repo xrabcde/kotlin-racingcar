87cc5e57f68d14a820212af366434348b92c64a4
xrabcde <xrabcde@gmail.com>
2022-06-09 21:06:08 +0900

    feat: 사용자 자동차이름, 이동횟수 입력기능 구현

---
+++ src/main/kotlin/racingcar/RacingcarApplication.kt
@@ -0,0 +1,10 @@
+package racingcar
+
+fun main() {
+    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    val names = readln()
+    println("시도할 횟수는 몇 회인가요?")
+    val turn = readln()
+    println("\n실행 결과")
+    println("가 최종 우승했습니다.")
+}
