24f4ef29c5f0194262883d029c05e8c879449a67
xrabcde <xrabcde@gmail.com>
2022-06-09 20:24:58 +0900

    feat: 5글자를 초과하는 자동차 이름 예외처리

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,3 +1,7 @@
 package racingcar
 
-class Car(val name: String)
+class Car(val name: String) {
+    init {
+        require(name.trim().length <= 5) { "자동차의 이름은 5글자 미만이어야 합니다." }
+    }
+}
--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,5 +1,6 @@
 package racingcar
 
+import io.kotest.assertions.throwables.shouldThrowWithMessage
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
@@ -7,5 +8,9 @@ class CarTest : FreeSpec({
     "자동차는 이름을 가질 수 있다" {
         val car = Car("bada")
         car.name shouldBe "bada"
+    }
+
+    "자동차의 이름은 5글자를 초과할 수 없다" {
+        shouldThrowWithMessage<IllegalArgumentException>("자동차의 이름은 5글자 미만이어야 합니다.") { Car("5글자가 넘는 이름") }
     }
 })
